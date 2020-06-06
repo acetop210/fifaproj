4b91c3130c4cd988bb27b2d38b877298a7da0174
acetop210 <acetop210@sasa.hs.kr>
2020-06-06 21:20:55 +0900

    sppo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMzAyNDcxMzk4IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjIwMDAwOjEwIiwibmJmIjoxNTkxNDQzNzEzLCJleHAiOjE2NTQ1MTU3MTMsImlhdCI6MTU5MTQ0MzcxM30.3udmK-WR4ohpfNL4tVLcN9swQE7b74tOUtMy63k1ZqQ";
+let sppo;
 function setup() {
   createCanvas(300, 300);
+  sppo = loadJSON("https://static.api.nexon.co.kr/fifaonline4/latest/spposition.json");
 }
 
 function draw() {
   background(255);
+  println(sppo);
 }
